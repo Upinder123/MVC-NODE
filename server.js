b9de05a87e2a9c1fafe6c5c3ce6a5b8fdcec7d4f
upinder <upinderjits@gmail.com>
2020-05-10 09:22:56 +0530

    models

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const Routes = require("./routes")
 const Hapi = require("@hapi/hapi")
+const Models = require("./Models/models")
 const init = async() =>{
     const server = new Hapi.Server({port:Settings.port});
 
 
 server.route(Routes);
 
+await Models.sequelize.sync();
+
 await server.start();
 console.log(`Server running at: ${server.info.uri}`);
+
 };
 process.on("unhandleRejection", err=>{
     console.log(err);
