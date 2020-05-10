2a26a6652860d9883a4ef4698e45412d9432a44b
upinder <upinderjits@gmail.com>
2020-05-10 09:04:50 +0530

    routes

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
-
+const Routes = require("./routes")
 const Hapi = require("@hapi/hapi")
 const init = async() =>{
     const server = new Hapi.Server({port:Settings.port});
 
 
-server.route({
-    method:"GET",
-    path:"/",
-
-    handler:(request,h) => {
-        return "Hello,World!";
-        
-    }
-});
+server.route(Routes);
 
 await server.start();
 console.log(`Server running at: ${server.info.uri}`);
